ee7c2a3f900e60d83a5612e03b382981c305cbd7
RasmusLumholdt <dev.rlumholdt@gmail.com>
2018-12-01 21:55:27 +0100

    Deatail site is done

--- src/App.js
+++ src/App.js
@@ -53,7 +53,7 @@ class App extends Component {
             <Route exact path="/" render={() => <Home username={this.state.username} LoggedIn={this.state.LoggedIn} ApiFacade={facade} />} />
             <Route path="/login" render={() => <Login redirect={this.state.redirect} ApiFacade={facade} login={this.login} />} />
             <Route path="/mylist" render={() => <MyList LoggedIn={this.state.LoggedIn} username={this.state.username} ApiFacade={facade} />} />
-            <Route path="/details/:symbol" render={() => <StockDetail />} />
+            <Route path="/details/:symbol" render={(props) => <StockDetail {...props} LoggedIn={this.state.LoggedIn} username={this.state.username} ApiFacade={facade}/>} />
 
           </div>
         </Router>
--- src/StockDetail.js
+++ src/StockDetail.js
@@ -1,27 +1,74 @@
 import React, { Component } from 'react';
-import ApiFacade from './apiFacade'
+import { BrowserRouter as Router, Route, NavLink } from "react-router-dom";
 
 class StockDetail extends Component {
 
     constructor(props) {
         super(props);
-        this.state = { user: props.username, stock: "Empty" };
+        this.state = { user: props.username, stock: "empty", userList: [] };
     }
 
     componentDidMount = async() => {
         
-        let fetchRes = await this.props.ApiFacade.fetchData("/api/user/" + this.props.username + "/list/", true);
+        //Get the users symbollist if they are logged in.
+        if (this.props.LoggedIn) {
+            const userList = await this.props.ApiFacade.fetchData("/api/user/" + this.props.username + "/list/", true);
+            let symbols = userList.map((stock) => {
+                return stock.symbol});
+            this.setState({ userList: symbols });
+
+        }
+
+        let fetchRes = await this.props.ApiFacade.fetchData("/api/stocks/single/" + this.props.match.params.symbol, false);
         this.setState({ stock: fetchRes });
+        
+        
+        
+        
         console.log(this.state.stock);
+        console.log(this.props);
+    }
+
+    checkIfUserFollow = () => {
+        if (this.props.LoggedIn) {
+            return !this.state.userList.includes(this.props.match.params.symbol);
+        }
+
+
+        console.log(this.state.username)
+        return false;
     }
 
 
     render() {
+        if(this.state.stock === "empty") {
+            return (
+                <div>
+                    <h2>Loading...</h2>
+                </div>
+            )
+        }
+
+        const stock = this.state.stock;
         return(
             <div>
+                <h1>{stock.companyName}</h1>
+                <h3>{stock.symbol}</h3>
                 
+                
+                
+                
+                <img src={process.env.PUBLIC_URL + '/images/stock.jpg'} alt="StockImage" style={{ width: 500, height: 300 }}/>
+                
+                
+                {this.checkIfUserFollow() === true && //If 
+                    <h1>Follow this stock</h1>
+                }
+
+                {console.log(this.state.stock)}
             </div>
         )
+        
     }
 
 }

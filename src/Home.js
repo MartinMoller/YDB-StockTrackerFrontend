7f131451f107d4196ff2ac740da362126e760c9e
David-M-Nielsen <wick3dwick@gmail.com>
2018-12-05 22:24:43 +0100

    cleaned up home. added symbols where approprate. changed the used list on frontpage. Added colour change based on gain/loss

--- src/Home.js
+++ src/Home.js
@@ -4,14 +4,7 @@ import StockTable from './StockTable';
 class Home extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { gainers: [], losers: [], stockAdded: '' };
-    }
-
-    componentDidMount = async () => {
-        //const gainers = await this.props.ApiFacade.fetchData("/api/stocks/list/gainers", false);
-        //this.setState({ gainers: gainers });
-        const losers = await this.props.ApiFacade.fetchData("/api/stocks/list/losers", false);
-        this.setState({ losers: losers });
+        this.state = {stockAdded: '' };
     }
 
     addStockToFav = (stock) => {
@@ -23,11 +16,8 @@ class Home extends React.Component {
         return <div className="content">
                 <h4>{this.state.stockAdded}</h4>
 
-                <h1 className="winner">Top gainers</h1>
-                <StockTable ApiFacade={this.props.ApiFacade} url="/api/stocks/list/gainers" LoggedIn={this.state.LoggedIn} username={this.state.username}/>
-
-                <h1 className="loser">Top losers</h1>
-                <StockTable ApiFacade={this.props.ApiFacade} url="/api/stocks/list/losers" LoggedIn={this.state.LoggedIn} username={this.state.username}/>
+                <h1>Stocks in Focus</h1>
+                <StockTable ApiFacade={this.props.ApiFacade} url="/api/stocks/list/infocus" LoggedIn={this.state.LoggedIn} username={this.state.username}/>
         </div>
     }
 }
